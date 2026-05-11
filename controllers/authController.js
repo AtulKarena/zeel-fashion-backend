98fad03bfd2081a385f59d4b3cc233b9ee69f7a2
AtulKarena <atulkarena602@gmail.com>
2026-05-11 18:04:45 +0530

    Still fixing

--- controllers/authController.js
+++ controllers/authController.js
@@ -87,7 +87,6 @@ exports.login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       sameSite: "none",
-      domain: ".vercel.app",
       secure: true, // true in production (HTTPS)
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
