fc485150a98ed70610479512220a17c757ab20d5
AtulKarena <atulkarena602@gmail.com>
2026-05-11 12:13:02 +0530

    uuid package error solve

--- routes/payment.js
+++ routes/payment.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { v4: uuidv4 } = require("uuid");
+const uuid = await import("uuid");
+const uuidv4 = uuid.v4;
+
+
 const crypto = require("crypto");
 const Razorpay = require("razorpay");
 
