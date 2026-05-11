3df269ec8695291fd7ef3bb23fe303cf5d2e0a3c
AtulKarena <atulkarena602@gmail.com>
2026-05-11 12:41:39 +0530

    Add frontend live url on cros

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://zeel-fashion-frontend.vercel.app",
     credentials: true, // ⭐ REQUIRED
     sameSite: "none",
     secure: true, // (true only if HTTPS)
