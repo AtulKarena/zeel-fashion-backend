3af84c19c0e06aee2194efbd7702d09fc97d7876
AtulKarena <atulkarena602@gmail.com>
2026-05-11 15:20:16 +0530

    Update mongodb url and bug solve

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://atulkarena2903_db_user:<rCLB5ulC43RC32im>@cluster0.yzbgoxb.mongodb.net/zeel_fashion?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://atulkarena2903_db_user:rCLB5ulC43RC32im@cluster0.yzbgoxb.mongodb.net/zeel_fashion?retryWrites=true&w=majority");
     console.log("MongoDB Connected");
   } catch (error) {
     console.error(error);
