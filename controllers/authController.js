a89f161b00183881c9395f62d90c5e136a4e5dce
AtulKarena <atulkarena602@gmail.com>
2026-05-11 17:44:00 +0530

    Update cookie configuration

--- controllers/authController.js
+++ controllers/authController.js
@@ -86,6 +86,7 @@ exports.login = async (req, res) => {
     console.log("Generated JWT Token:", token); // Debugging line
     res.cookie("token", token, {
       httpOnly: true,
+      sameSite: "lax",
       secure: true, // true in production (HTTPS)
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
