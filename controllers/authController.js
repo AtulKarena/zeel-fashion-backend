4c58096274da81b45f026e156d901e4383a85f0e
AtulKarena <atulkarena602@gmail.com>
2026-05-11 18:09:32 +0530

    Still bug solving of cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -86,7 +86,7 @@ exports.login = async (req, res) => {
     console.log("Generated JWT Token:", token); // Debugging line
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "none",
+      sameSite: true,
       secure: true, // true in production (HTTPS)
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
