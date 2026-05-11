ae2e7f5fe31c3d61aef7c4fc0fc138df15653f56
AtulKarena <atulkarena602@gmail.com>
2026-05-11 18:02:49 +0530

    Cros bug fixing

--- server.js
+++ server.js
@@ -7,14 +7,19 @@ const app = express();
 
 connectDB();
 // app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
+
 app.use(
   cors({
     origin: "https://zeel-fashion.vercel.app",
-    credentials: true, // ⭐ REQUIRED
-  }),
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
+
+app.options("*", cors());
+app.use(express.json());
+app.use(cookieParser());
 // Routes
 app.use("/uploads", express.static("uploads"));
 app.use("/api/auth", require("./routes/authRoutes"));
