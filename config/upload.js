480b9df6d19d3cb2f656b9b0968e896209db50c9
AtulKarena <atulkarena602@gmail.com>
2026-05-12 18:32:57 +0530

    Undo privious changes

--- config/upload.js
+++ config/upload.js
@@ -31,7 +31,7 @@ import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
 
 cloudinary.config({
-  cloud_name: 'zeel',
+  cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
