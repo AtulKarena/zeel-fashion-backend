0c7911b20c85c374525578fd461a12096af1b16d
AtulKarena <atulkarena602@gmail.com>
2026-05-12 18:14:41 +0530

    Add error handler on router of product create

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,7 +3,20 @@ const controller = require("../controllers/productController");
 const auth = require("../middleware/authMiddleware");
 const upload = require("../config/upload");
 
-router.post("/", auth, upload.array("images", 5), controller.createProduct);
+router.post("/", auth, (req, res, next) => {
+  upload.array("images", 5)(req, res, function (err) {
+    if (err) {
+      console.error("UPLOAD ERROR:", err);
+
+      return res.status(500).json({
+        success: false,
+        error: err.message || err,
+      });
+    }
+
+    next();
+  });
+}, controller.createProduct);
 router.get("/inventory", auth, controller.getInventory);
 router.get("/home", controller.getProductsByCategory);
 router.get("/", controller.getProducts);
